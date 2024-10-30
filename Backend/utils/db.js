a8db5b51a174fe35c99ccdcd4151046d97483be1
nadeemtadvi <nadeemtadvi1@gmail.com>
2024-10-30 14:35:16 +0530

    make changes in env

--- Backend/utils/db.js
+++ Backend/utils/db.js
@@ -2,7 +2,7 @@ import mongoose  from "mongoose";
 
 const DBcon = async () => {
     try {
-        await mongoose.connect(process.env.MONGOBD_URL)
+        await mongoose.connect(process.env.MONGODB_URL)
         console.log('mongoDB connected');
         
         
